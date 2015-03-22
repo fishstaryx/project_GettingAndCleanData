74fc2b8692f4692b781ba8efba02ae9e5e9e4e70
Yu Xin <fishstaryx@139.com>
2015-03-22 22:15:49 +0800

    my answer to the project of Getting and Cleaning Data course

---
+++ run_analysis.R
@@ -0,0 +1,47 @@
+#by yuxin
+
+library(plyr)
+
+# Read the data from the train and test folders and combine them.
+
+TrainSet <- read.table("./UCI HAR Dataset/train/X_train.txt")
+TrainActivity <- read.table("./UCI HAR Dataset/train/y_train.txt")
+TrainSubject <-  read.table("./UCI HAR Dataset/train/subject_train.txt")
+TrainData <- cbind(TrainSubject, TrainActivity, TrainSet)
+
+TestSet <- read.table("./UCI HAR Dataset/test/X_test.txt")
+TestActivity <- read.table("./UCI HAR Dataset/test/y_test.txt")
+TestSubject <-  read.table("./UCI HAR Dataset/test/subject_test.txt")
+TestData <- cbind(TestSubject, TestActivity, TestSet)
+
+DataSet <- rbind(TrainData, TestData)
+
+
+# Appropriately labels the data set with descriptive variable names.
+
+featurename <- read.table("./UCI HAR Dataset/features.txt")
+names(DataSet) <- c("Subject", "Activity", as.vector(featurename$V2))
+
+
+# Extracts only the measurements on the mean and standard deviation for each measurement.
+
+DataSubset <- DataSet[,c(1,2,grep("mean\\(\\)|std\\(\\)", featurename$V2)+2)]
+
+
+# Uses descriptive activity names to name the activities in the data set
+
+activitylabel<- read.table("./UCI HAR Dataset/activity_labels.txt")
+activitylabel <- as.vector(activitylabel[,2])
+for(i in 1:nrow(DataSubset)){ 
+        num = as.numeric(DataSubset[i,2])
+        DataSubset[i,2] = activitylabel[num] 
+}
+
+
+# create a tidy data set  with the average of each variable for each activity and each subjec
+numcol <- ncol(DataSubset)
+TidyData <- ddply(DataSubset,.(Subject, Activity), function(x) colMeans(x[,3:numcol]))
+names(TidyData)[3:numcol] <- paste(names(TidyData)[3:numcol],"_average",sep="")
+
+write.table(TidyData,file="./tidydata.txt", row.name=FALSE)
+
